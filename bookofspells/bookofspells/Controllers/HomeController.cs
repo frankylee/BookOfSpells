1a9de5876e066a52c7dea112d01dbd2f752b8af7
frankylee kelly <frankyleekelly@gmail.com>
2020-11-26 13:22:08 -0800

    Nearly complete with styling and content of index.html

--- bookofspells/bookofspells/Controllers/HomeController.cs
+++ bookofspells/bookofspells/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace bookofspells.Controllers
             return View();
         }
 
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
