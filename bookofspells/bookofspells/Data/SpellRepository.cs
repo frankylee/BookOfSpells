b86c6bad89f353aa287722dda7abada80e6f9a9c
frankylee kelly <frankyleekelly@gmail.com>
2020-12-03 15:58:06 -0800

    Added CheckFilename to SpellRepo to convert img uploads to random selection; fixed Enchantment where User object was not included when using GetSpellTitle()

--- bookofspells/bookofspells/Data/SpellRepository.cs
+++ bookofspells/bookofspells/Data/SpellRepository.cs
@@ -20,7 +20,14 @@ namespace bookofspells.Data
         public IQueryable<Spell> Spell
         {
             get
-            {   // Get all Spell objects in the Spell DBContext
+            {
+                // check filename and swap to generator
+                foreach (Spell s in context.Spell)
+                {
+                    s.Filename = CheckFilename(s.Filename);
+                    context.SaveChanges();
+                }
+                // Get all Spell objects in the Spell DBContext
                 // and include the User object
                 return context.Spell.Include(s => s.User);
             }
@@ -28,6 +35,8 @@ namespace bookofspells.Data
 
         public void AddSpell(Spell spell)
         {
+            // check filename and swap to generator
+            spell.Filename = CheckFilename(spell.Filename);
             // store in database
             context.Spell.Add(spell);
             context.SaveChanges();
@@ -36,8 +45,39 @@ namespace bookofspells.Data
         public Spell GetSpellTitle(string title)
         {
             // find and return the first spell with matching title
-            Spell spell = context.Spell.FirstOrDefault(s => s.Title == title);
+            Spell spell = context.Spell.Include(s => s.User).FirstOrDefault(s => s.Title == title);
             return spell;
+        }
+
+
+        // Check the img filename and swap to random
+        private string CheckFilename(string filename)
+        {
+            string[] img = {
+                "devil-goat-with-sacred-geometry.png",
+                "mandala_sacred_ancient_geometry_vector_3732998.png",
+                "mandala_geometry_sacred_symbol_4242415.png"
+            };
+
+            bool isMatch = false;
+            for (int i = 0; i < img.Length; i++)
+            {
+                if (filename == img[i])
+                    return filename;
+            }
+
+            if (!isMatch)
+            {
+                Random gen = new Random();
+                int rand = gen.Next();
+                if (rand % 3 == 0)
+                    filename = img[2];
+                else if (rand % 2 == 0)
+                    filename = img[0];
+                else
+                    filename = img[1];
+            }
+            return filename;
         }
     }
 }
